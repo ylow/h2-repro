4772f62da71d2f8cd44a27227cabb52579bd8dc2
Amos Wenger <amoswenger@gmail.com>
2022-10-20 20:04:02 +0200

    repro!

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::{
-    convert::Infallible,
-    str::FromStr,
-    sync::{Arc, Mutex},
-    time::Duration,
-};
+use std::{convert::Infallible, str::FromStr, time::Duration};
 
 use hyper::{
     service::{make_service_fn, service_fn},
-    Body, Request, Response,
+    Body, Client, Method, Request, Response,
 };
 use tracing::info;
 use tracing_subscriber::{filter::Targets, layer::SubscriberExt, util::SubscriberInitExt};
@@ -31,49 +26,57 @@ async fn real_main() -> color_eyre::Result<()> {
     let addr = "[::]:6400".parse()?;
     let server = hyper::server::Server::bind(&addr)
         .http2_only(true)
+        .http2_max_concurrent_streams(100)
         .serve(make_service_fn(|_conn| async {
-            Ok::<_, Infallible>(service_fn(hello_world))
+            Ok::<_, Infallible>(service_fn(sample_endpoint))
         }));
+
+    let _server_jh = tokio::spawn(async move {
+        server.await.unwrap();
+    });
 
     info!("Listening on {addr:?}");
 
-    server.await?;
+    let client = Client::builder().http2_only(true).build_http::<Body>();
+
+    let (tx, mut rx) = tokio::sync::mpsc::channel(4096);
+
+    let total_reqs = 200;
+    for i in 0..total_reqs {
+        let req = Request::builder()
+            .uri("http://localhost:6400")
+            .method(Method::POST)
+            .body(vec![0u8; 256 * 1024].into())?;
+        let tx = tx.clone();
+        let client = client.clone();
+        tokio::spawn(async move {
+            let res = client.request(req).await.unwrap();
+            let _body = hyper::body::to_bytes(res.into_body()).await.unwrap();
+            _ = tx.send((i, ())).await;
+        });
+    }
+    drop(tx);
+
+    let mut complete_reqs = 0;
+    while let Some((i, _res)) = rx.recv().await {
+        complete_reqs += 1;
+        info!("Received response {i} ({complete_reqs}/{total_reqs} done)");
+    }
 
     Ok(())
 }
 
-struct State {
-    counter: usize,
-}
+async fn sample_endpoint(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let (_parts, req_body) = req.into_parts();
+    hyper::body::to_bytes(req_body).await.unwrap();
 
-lazy_static::lazy_static! {
-    static ref STATE: Arc<Mutex<State>> = Arc::new(Mutex::new(State { counter: 0 }));
-}
+    let (mut sender, res_body) = Body::channel();
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let (mut sender, body) = hyper::Body::channel();
-    let counter = {
-        let mut state = STATE.lock().unwrap();
-        let counter = &mut state.counter;
-        *counter += 1;
-        *counter
-    };
+    tokio::spawn(async move {
+        tokio::time::sleep(Duration::from_millis(100)).await;
+        sender.send_data("hi there".into()).await.unwrap();
+    });
 
-    if counter > 2 {
-        tokio::spawn(async move {
-            tokio::time::sleep(Duration::from_millis(500)).await;
-            sender.send_data("this should work!".into()).await.unwrap();
-        });
-    } else {
-        tokio::spawn(async move {
-            for _ in 0..3 {
-                sender.send_data("this will fail...".into()).await.unwrap();
-                tokio::time::sleep(Duration::from_millis(00)).await;
-            }
-            sender.abort();
-        });
-    }
-
-    let res = Response::new(body);
+    let res = Response::new(res_body);
     Ok(res)
 }
