24c7360a31fa8f2fd248698922510f8c0cc2dd89
Yucheng Low <2740522+ylow@users.noreply.github.com>
2023-04-06 10:09:45 -0700

    repro again

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::{convert::Infallible, net::TcpListener, str::FromStr, time::Duration};
-
 use color_eyre::eyre;
+use futures::prelude::stream::*;
 use hyper::{
     body::Bytes,
     client::HttpConnector,
     service::{make_service_fn, service_fn},
     Body, Client, Method, Request, Response,
 };
+use std::{convert::Infallible, net::TcpListener, str::FromStr, time::Duration};
 use tokio::sync::mpsc;
 use tracing::{error, info};
 use tracing_subscriber::{filter::Targets, layer::SubscriberExt, util::SubscriberInitExt};
@@ -29,15 +29,14 @@ async fn real_main() -> eyre::Result<()> {
 
     let h2_max_streams: u32 = std::env::var("H2_MAX_STREAMS")
         .map(|s| s.parse().unwrap())
-        .unwrap_or(50);
+        .unwrap_or(100);
     let h2_requests = std::env::var("H2_REQUESTS")
         .map(|s| s.parse().unwrap())
-        .unwrap_or(100);
+        .unwrap_or(6000);
 
     info!("{h2_requests} requests on {h2_max_streams} streams");
     info!("(Set $H2_REQUESTS and $H2_MAX_STREAMS environment variables to adjust)");
 
-    run_test(false, h2_max_streams, h2_requests).await?;
     run_test(true, h2_max_streams, h2_requests).await?;
 
     Ok(())
@@ -70,7 +69,23 @@ async fn run_test(h2_only: bool, h2_max_streams: u32, h2_requests: u32) -> eyre:
         _ = hyper::body::to_bytes(res.into_body()).await?;
         Ok(())
     }
+    let mut strm = iter((0..h2_requests).map(|_| {
+        let req = Request::builder()
+            .uri(format!("http://{addr}"))
+            .method(Method::POST)
+            .body(Body::from(body.clone()))
+            .unwrap();
+        do_one_request(req, client.clone())
+    }))
+    .buffered(100);
 
+    let mut ctr: u64 = 0;
+    while let Some(resp) = strm.next().await {
+        ctr += 1;
+        eprintln!("{ctr} done");
+        tx.send(resp).await;
+    }
+    /*
     for _ in 0..h2_requests {
         let req = Request::builder()
             .uri(format!("http://{addr}"))
@@ -79,7 +94,7 @@ async fn run_test(h2_only: bool, h2_max_streams: u32, h2_requests: u32) -> eyre:
         let fut = do_one_request(req, client.clone());
         let tx = tx.clone();
         tokio::spawn(async move { _ = tx.send(fut.await).await });
-    }
+    }*/
     drop(tx);
 
     let mut complete_reqs = 0;
